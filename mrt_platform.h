0a3b34e0c12a8971622f37d27b78ceb7b1a05252
Jason Berger <berge472@gmail.com>
2019-03-13 09:09:57 -0400

    added in parallel abstraction

--- mrt_platform.h
+++ mrt_platform.h
@@ -5,18 +5,34 @@
   *@date 3/6/2019
   */
 
+typedef uint32_t mrt_status_t;
+
 #ifdef MRT_PLATFORM_STM32_HAL
+  #define MRT_PLATFORM "STM32"
   #include "Platforms/Stm32_HAL/stm32_hal_abstract.h"
 #endif
 
 #ifdef MRT_PLATFORM_ATMEL_START
+  #define MRT_PLATFORM "ATMEL_START"
   #include "Platforms/AtmelStart/atmel_start_abstract.h"
 #endif
 
 #ifdef MRT_PLATFORM_ATMEL_ASF
+  #define MRT_PLATFORM "ATMEL_ASF"
   #include "Platforms/AtmelStart/atmel_asf_abstract.h"
 #endif
 
+#ifdef MRT_PLATORM_CUSTOM
+  #define MRT_PLATFORM "CUSTOM"
+  #message "Platform is set to custom, be sure to provide a customized 'custom_platform.h' file"
+  #include "custom_platform.h"
+#endif
+
+#define MRT_STATUS_OK 0
+#define MRT_STATUS_ERROR 1
+
+#define MRT_HIGH 1
+#define MRT_LOW 0
 
 //platform_check must be included AFTER the abstraction header
 #include "platform_check.h"
--- platform_check.h
+++ platform_check.h
@@ -5,47 +5,79 @@
   *@date 3/6/2019
   */
 
+#define NOT_DEFINED_WARNING " was not defined in the current platform abstraction"
+
+#ifdef MRT_PLATFORM
+  #message "MrT Platform: " + MRT_PLATFORM
+#endif
+#ifndef MRT_PLATFORM
+  #error "No platform abstraction is defined!"
+#ifdef
+
+
   //Delay Abstraction
 #ifndef MRT_DELAY_MS
-  #warning "MRT_DELAY_MS Macro not provided in the current platform abstraction layer "
+  #define MRT_DELAY_MS( ... )
+  #warning "MRT_DELAY_MS" NOT_DEFINED_WARNING
 #endif
 
   //Uart Abstraction
 #ifndef MRT_UART_TX
-  #warning "MRT_UART_TX Macro not provided in the current platform abstraction layer "
+  #define MRT_UART_TX( ... )
+  #warning "MRT_UART_TX" NOT_DEFINED_WARNING
 #endif
 #ifndef MRT_UART_RX
-  #warning "MRT_UART_RX Macro not provided in the current platform abstraction layer "
+  #define MRT_UART_RX( ... )
+  #warning "MRT_UART_RX" NOT_DEFINED_WARNING
 #endif
 
   //GPIO Abstraction
 #ifndef MRT_GPIO_WRITE
-  #warning "MRT_GPIO_WRITE Macro not provided in the current platform abstraction layer "
+  #define MRT_GPIO_WRITE( ... )
+  #warning "MRT_GPIO_WRITE" NOT_DEFINED_WARNING
 #endif
 #ifndef MRT_GPIO_READ
-  #warning "MRT_GPIO_READ Macro not provided in the current platform abstraction layer "
+  #define MRT_GPIO_READ( ... )
+  #warning "MRT_GPIO_READ" NOT_DEFINED_WARNING
 #endif
 
   //I2C Abstraction
 #ifndef MRT_I2C_MASTER_TRANSMIT
-  #warning "MRT_I2C_MASTER_TRANSMIT Macro not provided in the current platform abstraction layer "
+  #define MRT_I2C_MASTER_TRANSMIT( ... )
+  #warning "MRT_I2C_MASTER_TRANSMIT" NOT_DEFINED_WARNING
 #endif
 #ifndef MRT_I2C_MASTER_RECEIVE
-  #warning "MRT_I2C_MASTER_RECEIVE Macro not provided in the current platform abstraction layer"
+  #define MRT_I2C_MASTER_RECEIVE( ... )
+  #warning "MRT_I2C_MASTER_RECEIVE" NOT_DEFINED_WARNING
 #endif
 #ifndef MRT_I2C_MEM_WRITE
-  #warning "MRT_I2C_MEM_WRITE Macro not provided in the current platform abstraction layer "
+  #define MRT_I2C_MEM_WRITE( ... )
+  #warning "MRT_I2C_MEM_WRITE" NOT_DEFINED_WARNING
 #endif
 #ifndef MRT_I2C_MEM_READ
-  #warning "MRT_I2C_MEM_READ Macro not provided in the current platform abstraction layer "
+  #define MRT_I2C_MEM_READ( ... )
+  #warning "MRT_I2C_MEM_READ" NOT_DEFINED_WARNING
 #endif
 
   //SPI Abstraction
 #ifndef MRT_SPI_TRANSFER
-  #warning "MRT_SPI_TRANSFER Macro not provided in the current platform abstraction layer "
+  #define MRT_SPI_TRANSFER( ... )
+  #warning "MRT_SPI_TRANSFER" NOT_DEFINED_WARNING
+#endif
+
+//Parallel Abstraction
+#ifndef MRT_8080_CMD
+  #define MRT_8080_CMD( ... )
+  #warning "MRT_8080_CMD" NOT_DEFINED_WARNING
+#endif
+
+#ifndef MRT_8080_DATA
+  #define MRT_8080_DATA( ... )
+  #warning "MRT_8080_DATA" NOT_DEFINED_WARNING
 #endif
 
   //printf
 #ifndef MRT_PRINTF
-  #warning "MRT_PRINTF Macro not provided in the current platform abstraction layer "
+  #define MRT_PRINTF( ... )
+  #warning "MRT_PRINTF" NOT_DEFINED_WARNING
 #endif
